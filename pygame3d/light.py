f694ed4b2d0e2b024076181ac1e477d5cba2bb95
Fr5ctal <fr5ctal@gmail.com>
2025-05-21 20:52:30 +0800

    Adding lights

---
+++ pygame3d/light.py
@@ -0,0 +1,31 @@
+from pygame3d.entity import Entity
+from panda3d.core import PointLight, DirectionalLight, AmbientLight, NodePath
+from typing import Literal
+
+
+class Light(Entity):
+    """
+    Light class for lights and illumination in the 3D surface
+    """
+    def __init__(self, type: Literal['point', 'directional', 'ambient']):
+        if type == 'point':
+            light = PointLight('plight')
+        elif type == 'directional':
+            light = DirectionalLight('dlight')
+        elif type == 'ambient':
+            light = AmbientLight('ablight')
+        else:
+            raise Exception(f'Invalid light type: {type}')
+
+        light = NodePath(light)
+        super().__init__(light)
+
+        self.core.parent.get_top().set_light(self.core)
+
+    @property
+    def color(self):
+        return self.core.node().get_color()
+
+    @color.setter
+    def color(self, color: tuple):
+        self.core.node().set_color(color)
